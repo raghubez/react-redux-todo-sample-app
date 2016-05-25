abce30e00e1730d14967cfe3a145a6d1a0784921
Raghu Bezawada <raghu.bezawada@gmail.com>
2016-05-25 08:34:39 +0100

    React Redux Todo Sample app

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  quiet: true,
+  entry: [
+    'babel-polyfill',
+    './src/js/app'
+  ],
+  output: {
+    path: __dirname + '/dist/js/',
+    filename: 'app.min.js',
+    publicPath: '/js/'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel'],
+      include: path.join(__dirname, './src/js/')
+    }]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+       compress: {
+          warnings: false
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '"production"'
+    })
+  ]
+};
---
+++ webpack.dev.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'babel-polyfill',
+    './src/js/app'
+  ],
+  target: 'web',
+  devtool: 'eval-source-map',
+  output: {
+    path: __dirname + '/src/js/',
+    filename: 'app.min.js',
+    publicPath: '/js/'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel'],
+      include: path.join(__dirname, './src/js/')
+    }]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      'Promise': 'es6-promise'
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '"development"'
+    })
+  ]
+};
